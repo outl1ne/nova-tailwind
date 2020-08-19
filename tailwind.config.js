1b4a7edc355a2ac1856fd68302df550479870536
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2020-08-19 18:34:10 +0300

    Fix shadows and text alignment

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   purge: false,
   theme: {
     extend: {},
+    boxShadow: {
+      xs: '0 0 0 1px rgba(0, 0, 0, 0.05)',
+      sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
+      xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
+      '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
+    },
   },
   variants: {},
   plugins: [],
@@ -12,6 +18,8 @@ module.exports = {
     fontSize: false,
     fontFamily: false,
     fontWeight: false,
+    lineHeight: false,
+    textAlign: false,
   },
   options: {
     prefix: '',
