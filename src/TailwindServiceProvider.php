ba9de700adcfb447cfe6b47e6d1bc7b64d830fef
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2020-08-19 18:07:43 +0300

    Oopsie

--- src/TailwindServiceProvider.php
+++ src/TailwindServiceProvider.php
@@ -6,7 +6,7 @@ use Laravel\Nova\Nova;
 use Laravel\Nova\Events\ServingNova;
 use Illuminate\Support\ServiceProvider;
 
-class FieldServiceProvider extends ServiceProvider
+class TailwindServiceProvider extends ServiceProvider
 {
     public function boot()
     {
