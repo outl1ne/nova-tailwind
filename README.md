ef6e3dedc433a7f10b29f748994ef35ffceaea39
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2021-01-21 13:26:29 +0200

    Add auth screens support

--- README.md
+++ README.md
@@ -23,6 +23,18 @@ The Tailwind configuration has been modified so it doesn't conflict with Laravel
 
 **If you do find some styles being overridden after installing this package, do make an issue and it can definitely be resolved.**
 
+## Custom cases
+
+### Usage on login screen
+
+Nova does not load the registered styles on the Login screen automatically. In order for the Tailwind to work on the login screen, you have to publish the CSS yourself. This package will then check if there's a published CSS file and will load it on the login screen as a Nova theme style.
+
+Basically, just publish the CSS and it will work on the auth screens:
+
+```bash
+php artisan vendor:publish --tag="tailwind-public-css"
+```
+
 ## Credits
 
 - [Tarvo Reinpalu](https://github.com/Tarpsvo)
--- src/TailwindServiceProvider.php
+++ src/TailwindServiceProvider.php
@@ -10,8 +10,17 @@ class TailwindServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        Nova::serving(function (ServingNova $event) {
-            Nova::style('nova-tailwind', __DIR__ . '/../dist/tailwind.css');
-        });
+        $tailwindPublicPath = public_path('vendor/nova-tailwind');
+
+        if (file_exists("{$tailwindPublicPath}/tailwind.css")) {
+            Nova::theme('/vendor/nova-tailwind/tailwind.css');
+        } else {
+            Nova::serving(function (ServingNova $event) {
+                Nova::style('nova-tailwind', __DIR__ . '/../dist/tailwind.css');
+            });
+        }
+
+        // Publish CSS for login screen
+        $this->publishes([__DIR__ . '/../dist' => $tailwindPublicPath], 'tailwind-public-css');
     }
 }
