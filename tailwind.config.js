aa80a122c9a32ce32b2c97642e5349efd4fdb5c2
Nattapong Mongkolnavin <nattapong@octagon.co.th>
2021-11-11 13:38:48 +0700

    Update tailwind.config.js
    Adding tailwind's curating colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: false,
   theme: {
     extend: {},
+    colors: colors,
     boxShadow: {
       xs: '0 0 0 1px rgba(0, 0, 0, 0.05)',
       sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
